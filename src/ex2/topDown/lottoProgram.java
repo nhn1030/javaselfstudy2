6ebda3ba7ef51e419c840fd7f5acc4f12488a913
nhn1030 <shgusska12@gmail.com>
2023-07-11 22:59:19 +0900

    first commit

---
+++ src/ex2/topDown/lottoProgram.java
@@ -0,0 +1,52 @@
+package ex2.topDown;
+
+public class lottoProgram {
+    public static void main (String[] args) {
+
+        int [][] lottos = null;
+        int menu;
+        boolean running = true;
+
+
+        while (running) {
+            menu = inputMenu();
+
+            switch (menu) {
+
+                case 1:
+                    lottos = createLottosAuto();
+                    break;
+
+                case 2:
+                    lottos = createLottosManual();
+                    break;
+
+                case 3:
+                    printLottos(lottos);
+                    break;
+
+                case 4:
+                    running = false;
+                    break;
+
+                default:
+        }
+
+
+
+        }
+    }
+
+    private static void printLottos(int[][] lottos) {
+    }
+
+    private static int[][] createLottosManual() {
+    }
+
+    private static int[][] createLottosAuto() {
+    }
+
+    private static int inputMenu() {
+
+    }
+}
