41bef266ddaea7bc39417d1eb9c3c6e3e2591368
nhn1030 <shgusska12@gmail.com>
2023-07-14 14:41:17 +0900

    first commit

---
+++ src/ex3/parameter/exercise.java
@@ -0,0 +1,13 @@
+package ex3.parameter;
+
+public class exercise {
+    public static void main (String[] args) {
+
+        char[][] names = new char[3][10];
+        double cnt = print(true, 4.0, names);
+
+        static double print(boolean b, double f, names) {
+            return 2.2;
+        }
+    }
+}
