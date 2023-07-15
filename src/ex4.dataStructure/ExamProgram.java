e4f2cf1d29b1ea8883ea8e352983763684dbac28
nhn1030 <shgusska12@gmail.com>
2023-07-15 13:38:09 +0900

    revise doc

---
+++ src/ex4.dataStructure/ExamProgram.java
@@ -0,0 +1,8 @@
+package ex4.dataStructure;
+
+public class ExamProgram {
+    int kor;
+    int eng;
+    int math;
+
+}
--- src/ex4.dataStructure/structure_code.java
+++ src/ex4.dataStructure/structure_code.java
@@ -1,87 +1,79 @@
-package ex1.bottomUp;
+package ex4.dataStructure;
 import java.util.Scanner;
 
 public class structure_code {
-
-
-    static int[] korlist = new int[3]; // 국어 성적 배열
-
     public static void main(String[] args) {
 
+        ExamProgram exam;
+        exam = new ExamProgram();
+
+        input(exam);
+
+        print(exam);
+    }
+
+    private static void print(ExamProgram exam) {
+
         Scanner scan = new Scanner(System.in);
-
-
-        while (true) {
-            // ------ 메뉴 선택 부분 -------
-            System.out.println("\n+---------------------------------+");
-            System.out.println("|             메인 메뉴              |");
-            System.out.println("+---------------------------------+");
-            System.out.println("\t1.성적입력 ");
-            System.out.println("\t2.성적출력 ");
-            System.out.println("\t3.종료 ");
-            System.out.print("\t> ");
-            int menu = scan.nextInt();
-
-            switch (menu) {
-
-                // ------ 성적 입력 부분 -------
-                case 1:
-                    inputGrades(korlist, scan);
-                    break;
-                // ------ 성적 출력 부분 -------
-                case 2:
-                    printGrades(korlist);
-                    break; // Add break statement here
-
-                case 3:
-                    System.out.println("프로그램을 종료합니다.");
-                    return;
-
-                default:
-                    System.out.println("입력 오류!");
-                    break;
-            }
-        }
-    }
-
-    static void inputGrades(int[] kors, Scanner scan) {
-
-        System.out.println("\n+---------------------------------+");
-        System.out.println("|             성적 입력              |");
-        System.out.println("+---------------------------------+");
-
-        for (int i = 0; i < kors.length; i++) {
-            do {
-                System.out.printf("국어%d: ", i + 1);
-                kors[i] = scan.nextInt();
-
-                if (kors[i] < 0 || 100 < kors[i])
-                    System.out.println("성적범위(0 ~ 100)를 벗어났습니다.");
-            } while (kors[i] < 0 || 100 < kors[i]);
-        }
-        System.out.println("성적이 정상 입력되었습니다.");
-    }
-
-    static void printGrades(int[] kors) {
-
-        int total = 0;
 
         System.out.println("+---------------------------------+");
         System.out.println("|             성적출력              |");
         System.out.println("+---------------------------------+");
+        System.out.println();
 
-        for (int i = 0; i < kors.length; i++) {
-            System.out.printf("\t국어%d: %3d\n", i + 1, kors[i]);
-            total += kors[i];
-        }
+        int kor = exam.kor;
+        int eng = exam.eng;
+        int math = exam.kor;
 
-        float avg = (float) total / kors.length;
+        int total = kor + eng + math;
+        float avg = total/3.0f;
 
-        System.out.printf("\t총점: %3d", total);
-        System.out.printf("\t평균: %6.2f", avg);
-        System.out.println("\n--------------------------------------");
-
-
+        System.out.printf("국어 : %d\n", kor);
+        System.out.printf("영어 : %d\n", eng);
+        System.out.printf("수학 : %d\n", math);
+        System.out.printf("총점 : %3d\n", total);
+        System.out.printf("평균 : %6.2f\n", avg);
     }
 
+    private static void input(ExamProgram exam) {
+
+        System.out.println("+---------------------------------+");
+        System.out.println("|             성적입력              |");
+        System.out.println("+---------------------------------+");
+        System.out.println();
+
+        Scanner scan = new Scanner(System.in);
+
+        String[] subjects = {"국어", "영어", "수학"};
+
+
+        for (int i = 0; i < subjects.length; i++) {
+
+            int score;
+
+            do {
+                System.out.printf(subjects[i] + "점수 입력 : ");
+                score = scan.nextInt();
+
+                if (score < 0 || 100 < score) {
+                    System.out.println("성적 범위를 벗어났습니다. 다시 입력하세요.");
+                }
+            } while (score < 0 || 100 < score);
+
+            switch (i) {
+                case 0:
+                    exam.kor = score;
+                    break;
+
+                case 1:
+                    exam.eng = score;
+                    break;
+
+                case 2:
+                    exam.math = score;
+                    break;
+            }
+        }
+        System.out.println("성적이 정상 입력 되었습니다.");
+    }
 }
